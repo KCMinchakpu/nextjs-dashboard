5746c1d7619dd94eec0de6f5cb5f90b6db10cc52
KEVIN CROSS MINCHAKPU <113268445+KCMinchakpu@users.noreply.github.com>
2024-10-10 18:55:47 +0100

    update
    update

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,3 @@ const nextConfig = {
 
 export default nextConfig;
 
-module.exports = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'development' 
-      ? 'http://localhost:3000' 
-      : 'https://nextjs-dashboard-eight-lake-62.vercel.app/',
-  },
-};
